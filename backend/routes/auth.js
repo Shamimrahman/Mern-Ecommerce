d7902c7762227a3b98dd05a7af98265ca327c398
unknown <shamim.rahman@northsouth.edu>
2022-01-19 03:09:57 +0600

    logout

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controller/authController");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controller/authController");
 
 //create user route
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").get(logout);
 
 module.exports = router;
