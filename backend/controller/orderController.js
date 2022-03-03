17423c9cc6add73a2fd7155f54754c90e2c85ca3
unknown <shamim.rahman@northsouth.edu>
2022-03-03 23:51:16 +0600

    updated singleorder

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -33,3 +33,31 @@ exports.newOrder = asyncCatchError(async (req, res, next) => {
     order,
   });
 });
+
+//get single order =>api/v1/order/:id
+
+exports.getSingleOrder = asyncCatchError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (!order) {
+    return next(new ErrorHandler("No order found with this id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+//get logeed in user orders=>api/v1/orders/me
+
+exports.myOrders = asyncCatchError(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
