3d79f37df6fa2c158a91165ab79043a22f4b194d
unknown <shamim.rahman@northsouth.edu>
2022-04-05 03:55:50 +0600

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Home from "./Homepage/Home";
 
 const App = () => {
   return (
     <div>
       <h1>I am App</h1>
+      <Home></Home>
     </div>
   );
 };
--- frontend/src/Homepage/Home.jsx
+++ frontend/src/Homepage/Home.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
-function Home() {
-  return <div>Home</div>;
-}
-
+import React, { Fragment } from "react";
+import home from "./Home.css";
+export const Home = () => {
+  return (
+    <Fragment>
+      <div className="home">Home</div>
+    </Fragment>
+  );
+};
 export default Home;
